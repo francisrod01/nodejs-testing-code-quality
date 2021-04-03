bdad57005869ad5ca6b1741808492bac112431da
Francis Rodrigues <francis.rod01@outlook.com>
2021-04-03 19:06:59 -0300

    wip(test): Stubbing a custom response
    Fake database response in Create context

--- test/unit/lib/reservations.js
+++ test/unit/lib/reservations.js
@@ -3,6 +3,7 @@ const sinon = require('sinon');
 const chai = require('chai');
 const should = chai.should();
 const Reservation = require('../../../lib/schema/reservation');
+const db = require('sqlite');
 
 describe('Reservations Library', function() {
   const debugStub = function() {
@@ -43,6 +44,47 @@ describe('Reservations Library', function() {
 
       return reservations.validate(reservation)
         .catch(error => error.should.be.an('error').and.not.be.null);
+    });
+  });
+
+  context('Create', function() {
+    let dbStub;
+
+    before(function() {
+      // @ts-ignore
+      dbStub = sinon.stub(db, 'run').resolves({
+        // @ts-ignore
+        stmt: {
+          lastID: 1349
+        }
+      });
+
+      reservations = proxyquire('../../../lib/reservations', {
+        debug: debugStub,
+        sqlite: dbStub
+      });
+    });
+
+    after(function() {
+      dbStub.restore();
+    });
+
+    it('should return the created reservation ID', function(done) {
+      // @ts-ignore
+      const reservation = new Reservation({
+        date: '2017/06/10',
+        time: '06:02 AM',
+        party: 4,
+        name: 'Family',
+        email: 'username@example.com'
+      });
+
+      reservations.create(reservation)
+        .then(lastID => {
+          lastID.should.deep.equal(1349);
+          done();
+        })
+        .catch(error => done(error));
     });
   });
 });
